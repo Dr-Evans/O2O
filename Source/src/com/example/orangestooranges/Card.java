1ac09a5c8210a1c6bcab9bb4a7384dffc90a936e
Kirk Henf <kjhenf@gmail.com>
2014-02-13 16:10:18 -0500

    Adding basic card framework (with Orange and Blues being children of the Card superclass)

---
+++ Source/src/com/example/orangestooranges/Card.java
@@ -0,0 +1,17 @@
+package com.example.orangestooranges;
+
+public class Card {
+	//variables
+	public int card_ID; //unique ID of card
+	public String ctopic; //card topic and main subject
+	public String cdes; //extended card description
+	
+	Card() {}
+	
+	Card(int _card_ID, String _ctopic, String _cdes) {
+		this.card_ID = _card_ID;
+		this.ctopic = _ctopic;
+		this.cdes = _cdes;
+	}
+	
+}
