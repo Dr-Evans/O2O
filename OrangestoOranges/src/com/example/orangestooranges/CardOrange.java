6e236d80faf595c2523eeae49a1c33692bbca820
Nick Evans <nickevans@ufl.edu>
2014-02-19 17:45:52 -0500

    this should fix it

---
+++ OrangestoOranges/src/com/example/orangestooranges/CardOrange.java
@@ -0,0 +1,15 @@
+package com.example.orangestooranges;
+
+public class CardOrange extends Card {
+	
+	//default constructor
+	CardOrange() {
+		super();
+	}
+	
+	//calls Card class superconstructor
+	CardOrange(int _card_ID, String _ctopic, String _cdes) {
+		super(_card_ID, _ctopic, _cdes);
+	}	
+
+}
