89f185c76965d9b2690dc89f95f216964f57b6d7
Nick Evans <nickevans@ufl.edu>
2014-04-20 04:32:17 -0400

    splash screen works but is slow

---
+++ src/com/example/orangestooranges/StartUp.java
@@ -0,0 +1,32 @@
+package com.example.orangestooranges;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+ 
+public class StartUp extends Activity {
+ 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+ 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(StartUp.this, LoginActivity.class);
+                startActivity(i);
+ 
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+ 
+}
