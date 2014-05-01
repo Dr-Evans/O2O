bebb920440c835c546f428573c7e67306fd5f973
Kevin Liu <kevinliu314@gmail.com>
2014-05-01 14:47:41 -0400

    added functionality to the endscreen to display winner, prepepd framework for adding a sorted by score end screen

--- src/com/example/orangestooranges/EndScreen.java
+++ src/com/example/orangestooranges/EndScreen.java
@@ -1,8 +1,11 @@
 package com.example.orangestooranges;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -26,13 +29,23 @@ public class EndScreen extends Activity {
 	    p.setMargins(50,20,50,0);
 		TextView temp; 
 		LinearLayout scoreBoard = (LinearLayout) findViewById(R.id.scores);
+		//ArrayList<TextView> scores = new ArrayList<TextView>();
 		for (int i = 0; i < numPlayers; i++) {
 				temp = new TextView(this);
 			    temp.setText(results.players.get(i).username +"'s Score: "+ results.getPlayer(i).getPoints());
 			    //Set text view needs to be set to the username of the player in the future
 			    temp.setId(i);
+			    if(results.getPlayer(i).getPoints()==results.maxScore){
+			    	temp.setTextSize(35);
+			    	temp.setTextColor(Color.GREEN);
+			    }
+			    else{
+				    temp.setTextSize(20);
+			    }
+			    //scores.add(temp);
 			    scoreBoard.addView(temp,p);
 		}
+		
 		
 	}
 	
